7045c91c8ebadc2a23fb4d116ea601ddea29b0c1
2003Vandu <2003Vandu>
2026-01-28 00:42:50 +0530

    customerForm add

--- src/Componesnts/CustomerForm/CustomerForm.jsx
+++ src/Componesnts/CustomerForm/CustomerForm.jsx
@@ -1,8 +1,24 @@
 import './CustomerForm.css'
 
-const CustomerForm=()=>{
+const CustomerForm=({custemerName, setCustomerName,mobileNumber, setMobileNumber})=>{
     return(
-        <div>Customer Form</div>
+        <div className="p-3">
+            <div className="mb-3">
+                <div className="d-flex align-items-center gap-2">
+                    <label htmlFor="customerName"className="col-5 text-white" >Customer Name :</label>
+                    <input type="text" className="form-control form-control-sm" id="customerName" 
+                    onChange={(e)=>setCustomerName(e.target.value)} value={custemerName}/>
+                </div>
+            </div>
+            <div className="mb-3">
+                <div className="d-flex align-items-center gap-2">
+                    <label htmlFor="MobileNumber"className="col-5 text-white">Mobile Number :</label>
+                    <input type="text" className="form-control form-control-sm" id="MobileNumber" 
+                    onChange={(e)=>setMobileNumber(e.target.value)} value={mobileNumber}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 export default CustomerForm;
