015724242caba401c0296486f74453fa0c61b991
2003Vandu <2003Vandu>
2026-01-28 00:50:14 +0530

    menuBar update

--- src/Componesnts/MenuBar/MenuBar.jsx
+++ src/Componesnts/MenuBar/MenuBar.jsx
@@ -27,7 +27,7 @@ const MenuBar = () =>{
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse p-2" id="navbarNav">
+    <div className="collapse navbar-collapse p-1" id="navbarNav">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
                 <Link className="nav-link" to="/dashboard">DashBoard</Link>
