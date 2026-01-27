4220ecaa3ff9e2cfe86e7ab95dbcbd8f89300cf4
2003Vandu <2003Vandu>
2026-01-28 00:45:15 +0530

    Item.jsx inti

--- src/Componesnts/Item/Item.jsx
+++ src/Componesnts/Item/Item.jsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import './Item.css'
+import { AppContext } from '../../Context/Appcontext';
 
 {/** hear we accept the prop of Items from parent  >> inside this we designe a Item component */}
 const Item = ({ itemName,itemprice,itemImage,itemid })=>{
+       const {addToCart} = useContext(AppContext);
+
+       const handlerAddToCart=()=>{
+        addToCart({
+          name: itemName,
+          price: itemprice,
+          quantity: 1,
+          itemid: itemid
+        })
+       }
+
     return(
-        <div className="p-3 bg-dark rounded shadow-sm h-100 d-flex align-items-center item-card">
+      <div className="p-3 bg-dark rounded shadow-sm h-100 d-flex flex-column flex-sm-row align-items-center item-card">
       <div style={{ position: "relative", marginRight: "15px" }}>
         <img src={itemImage} alt={itemName} className="item-image" />
       </div>
@@ -14,8 +27,8 @@ const Item = ({ itemName,itemprice,itemImage,itemid })=>{
       </div>
 
       <div className="d-flex flex-column justify-content-between align-items-center ms-3" style={{ height: "100%" }}>
-        <i className="bi bi-cart-plus fs-5 text-warning"></i>
-        <button className="btn btn-success btn-sm">
+        <i className="bi bi-cart-plus fs-5 text-warning" onClick={handlerAddToCart}></i>
+        <button className="btn btn-success btn-sm" onClick={handlerAddToCart}>
           <i className="bi bi-plus"></i>
         </button>
       </div>
