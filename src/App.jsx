675f2eb1221ddb13b0fbac1909516a7c53767fd1
2003Vandu <patilvandesh301@gmail.com>
2026-07-02 18:41:19 +0530

    feat: added routing protection and forced login page first

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import MenuBar from "./Componesnts/MenuBar/MenuBar";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import DashBoard from "./Pages/DashBoard/DashBoard";
 import ManageCategories from "./Pages/ManageCategories/ManageCategories";
 import ManageUsers from "./Pages/ManageUsers/ManageUsers";
@@ -8,11 +8,39 @@ import Manageitem from "./Pages/Manageitems/Manageitems";
 import Explore from "./Pages/Explore/Explore";
 import { Toaster } from "react-hot-toast"
 import Login from "./Pages/Login/Login";
+import OrderHistory from "./Componesnts/OrderHistory/OrderHistory";
+import { AppContext } from "./Context/Appcontext";
+import NotFound from "./Componesnts/NotFound/NotFound";
 
 function App() {
 
-  // Use to restrict thr location 
+  {/*Use to restrict thr location */}
   const location =  useLocation();
+
+  // use to make protect a page if admin or user is not login then no access to any page 
+  const {auth} = useContext(AppContext);
+
+  // hear we destructure the auth and get element 
+  const LoginRoute = ({element}) => {
+
+    // if user is logedin then we will not allow user to go back to login page 
+    if(auth.token){
+      return <Navigate to="/dashboard" replace/>
+    }
+    return element;
+  }
+
+  {/** allow only specific orle or admin */}
+  const ProtectedRoute =({element, allowedRoles}) =>{
+    if(!auth.token){
+      return <Navigate  to="/login" replace/>
+    }  
+    if (allowedRoles && !allowedRoles.includes(auth.role)) {
+      return <Navigate to="/dashboard" replace/>
+
+    }
+    return element;
+  }
 
   return (
     <div>
@@ -20,14 +48,23 @@ function App() {
       {location.pathname !=="/login"&&<MenuBar/>}
       <Toaster/>
       <Routes>
-        <Route path="/dashboard" element={<DashBoard/>}/>
-        <Route path="/category" element={<ManageCategories/>}/>
-        <Route path="/users" element={<ManageUsers/>}/>
-        <Route path="/items" element={<Manageitem/>}/>
-        <Route path="/explore" element={<Explore/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/" element={<DashBoard/>}/>
+        {/* All routes below are now wrapped in ProtectedRoute */}
+        <Route path="/dashboard" element={<ProtectedRoute element={<DashBoard />} />} />
+        <Route path="/explore" element={<ProtectedRoute element={<Explore />} />} />
         
+        {/* Admin only routes */}
+        <Route path="/category" element={<ProtectedRoute element={<ManageCategories />} allowedRoles={['ROLE_ADMIN']} />} />
+        <Route path="/users" element={<ProtectedRoute element={<ManageUsers />} allowedRoles={['ROLE_ADMIN']} />} />
+        <Route path="/items" element={<ProtectedRoute element={<Manageitem />} allowedRoles={['ROLE_ADMIN']} />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<LoginRoute element={<Login />} />} />
+        <Route path="/orders" element={<ProtectedRoute element={<OrderHistory />} />} />
+        
+        {/* Root path protected */}
+        <Route path="/" element={<ProtectedRoute element={<DashBoard />} />} />  
+        
+        <Route path="*" element={<NotFound />} />  
       </Routes>
     </div>
   );
